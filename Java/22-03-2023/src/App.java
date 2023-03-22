1d65ccf5bfd6d2d954d7a03d72c1235043b750fd
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-03-22 09:50:03 -0300

    commit

---
+++ Java/22-03-2023/src/App.java
@@ -0,0 +1,11 @@
+package aula7_construtores;
+
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private String curso;
+    private String modalidade;
+    private boolean status;
+    private float mediaGeral
+}
+
